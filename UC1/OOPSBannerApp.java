29427496d2ae93843b57df9c3cdde9acfce07c45
kamna Sharma <kamnasharma@kamnas-MacBook-Pro.local>
2026-03-03 15:08:32 +0530

    Add Java UC1 code

---
+++ UC1/OOPSBannerApp.java
@@ -0,0 +1,19 @@
+/**
+ * The OOPSBannerApp class is the entry point for the OOPS Banner Application.
+ * In this first use case (UC1), it simply prints the string "OOPS" to the console.
+ *
+ * @author Neil Sahay
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+
+    /**
+     * The main method – the JVM calls this method to start the program.
+     *
+     * @param args command-line arguments (not used in UC1)
+     */
+    public static void main(String[] args) {
+        // Print the literal "OOPS" to the console
+        System.out.println("OOPS");
+    }
+}
